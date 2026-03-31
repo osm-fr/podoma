fcfd63935a1e058b1ffe0845c4e58ff0c115aab0
Daniele Santini <danysan95@gmail.com>
2026-04-01 00:30:48 +0200

    Remove personal info from logs

--- website/index.js
+++ website/index.js
@@ -1011,7 +1011,6 @@ app.get("/users/:name", (req, res) => {
   }
 
   // Find user in database
-  console.log(`Getting user ID for username ${req.params.name}`);
   pool
     .query(`SELECT userid FROM pdm_user_names WHERE username = $1`, [
       req.params.name.replace(" ", "%20%"),
@@ -1031,7 +1030,6 @@ app.get("/users/:name", (req, res) => {
           ])
           .join(" UNION ALL ");
 
-        console.log(`Fetching badges for user ${userid}`);
         pool
           .query(sql, [userid])
           .then((res2) => {
